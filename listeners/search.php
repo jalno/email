d003e86f18037ad8f285d4940d38af3090f954ff
Mahdi Abedi <abedi@jeyserver.com>
2017-03-01 10:41:34 +0330

    Fix #1

--- listeners/search.php
+++ listeners/search.php
@@ -28,37 +28,37 @@ class search{
 		$types = authorization::childrenTypes();
 		$get_list_anonymous = authorization::is_accessed('get_list_anonymous');
 		$parenthesis = new parenthesis();
-		foreach(array('sender_number', 'receiver_number', 'text') as $item){
-			$parenthesis->where("sms_get.".$item,$word, 'contains', 'OR');
+		foreach(array('sender_address', 'receiver_address', 'text') as $item){
+			$parenthesis->where("email_get.{$item}", $word, 'contains', 'OR');
 		}
 		db::where($parenthesis);
 		if($get_list_anonymous){
-			db::join("userpanel_users", "userpanel_users.id=sms_get.sender_user", "left");
+			db::join("userpanel_users", "userpanel_users.id=email_get.sender_user", "left");
 			$parenthesis = new parenthesis();
 			$parenthesis->where("userpanel_users.type",  $types, 'in');
-			$parenthesis->where("sms_get.sender_user", null, 'is','or');
+			$parenthesis->where("email_get.sender_user", null, 'is','or');
 			db::where($parenthesis);
 		}else{
-			db::join("userpanel_users", "userpanel_users.id=sms_get.sender_user", "inner");
+			db::join("userpanel_users", "userpanel_users.id=sender_user.sender_user", "inner");
 			if($types){
 				db::where("userpanel_users.type", $types, 'in');
 			}else{
 				db::where("userpanel_users.id", authentication::getID());
 			}
 		}
-		db::orderBy('sms_get.id', 'DESC');
-		$items = db::get('sms_get', null, array("sms_get.*"));
+		db::orderBy('email_get.id', 'DESC');
+		$items = db::get('email_get', null, array("email_get.*"));
 		$gets = array();
 		foreach($items  as $item){
 			$gets[] = new get($item);
 		}
 		foreach($gets as $get){
 			$result = new link();
-			$result->setLink(userpanel\url('sms/get', array('id' => $get->id)));
-			$result->setTitle(translator::trans("sms.get.bySenderNumber", array(
-				'senderNumber' => $get->sender_number
+			$result->setLink(userpanel\url('email/get', array('id' => $get->id)));
+			$result->setTitle(translator::trans("email.get.bySenderAddress", array(
+				'senderAddress' => $get->sender_address
 			)));
-			$result->setDescription(translator::trans("sms.get.description", array(
+			$result->setDescription(translator::trans("email.get.description", array(
 				'receive_at' => date::format("Y/m/d H:i:s", $get->receive_at),
 				'text' => mb_substr($get->text, 0, 70)
 			)));
@@ -70,37 +70,37 @@ class search{
 		$types = authorization::childrenTypes();
 		$sent_list_anonymous = authorization::is_accessed('sent_list_anonymous');
 		$parenthesis = new parenthesis();
-		foreach(array('sender_number', 'receiver_number', 'text') as $item){
-			$parenthesis->where("sms_sent.".$item,$word, 'contains', 'OR');
+		foreach(array('sender_address', 'receiver_address', 'text') as $item){
+			$parenthesis->where("email_sent.{$item}", $word, 'contains', 'OR');
 		}
 		db::where($parenthesis);
 		if($sent_list_anonymous){
-			db::join("userpanel_users", "userpanel_users.id=sms_sent.receiver_user", "left");
+			db::join("userpanel_users", "userpanel_users.id=email_sent.receiver_user", "left");
 			$parenthesis = new parenthesis();
 			$parenthesis->where("userpanel_users.type",  $types, 'in');
-			$parenthesis->where("sms_sent.receiver_user", null, 'is','or');
+			$parenthesis->where("email_sent.receiver_user", null, 'is','or');
 			db::where($parenthesis);
 		}else{
-			db::join("userpanel_users", "userpanel_users.id=sms_sent.receiver_user", "inner");
+			db::join("userpanel_users", "userpanel_users.id=email_sent.receiver_user", "inner");
 			if($types){
 				db::where("userpanel_users.type", $types, 'in');
 			}else{
 				db::where("userpanel_users.id", authentication::sentID());
 			}
 		}
-		db::orderBy('sms_sent.id', 'DESC');
-		$items = db::get('sms_sent', null, array("sms_sent.*"));
+		db::orderBy('email_sent.id', 'DESC');
+		$items = db::get('email_sent', null, array("email_sent.*"));
 		$sents = array();
 		foreach($items  as $item){
 			$sents[] = new sent($item);
 		}
 		foreach($sents as $sent){
 			$result = new link();
-			$result->setLink(userpanel\url('sms/sent', array('id' => $sent->id)));
-			$result->setTitle(translator::trans("sms.sent.byReceiverNumber", array(
-				'receiverNumber' => $sent->receiver_number
+			$result->setLink(userpanel\url('email/sent', array('id' => $sent->id)));
+			$result->setTitle(translator::trans("email.sent.byReceiverAddress", array(
+				'receiverAddress' => $sent->receiver_address
 			)));
-			$result->setDescription(translator::trans("sms.sent.description", array(
+			$result->setDescription(translator::trans("email.sent.description", array(
 				'send_at' => date::format("Y/m/d H:i:s", $sent->send_at),
 				'text' => mb_substr($sent->text, 0, 70)
 			)));
