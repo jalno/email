751fedf295c1bb6f0559105985033e121eaf6097
Amir.h Yeganemehr <yeganemehr@yahoo.com>
2017-05-31 15:08:38 +0430

    add an argument for target user

--- libraries/notifications/channel.php
+++ libraries/notifications/channel.php
@@ -5,9 +5,10 @@ use \packages\notifications;
 use \packages\email\api;
 class channel extends notifications\channel{
 	public function notify(event $event){
-		$api = new api();
-		$api->template($event->getName(), $event->getArguments());
 		foreach($event->getTargetUsers() as $user){
+			$api = new api();
+			$arguments = array_replace(array('user' => $user), $event->getArguments());
+			$api->template($event->getName(), $arguments);
 			$api->to($user->email, $user->getFullName());
 			$api->toUser($user);
 			$api->send();
