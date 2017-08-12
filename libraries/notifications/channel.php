f32f975ce0ce55c5b2acf2a2762f25516515c112
Mahdi Abedi <abedi@jeyserver.com>
2017-08-12 18:07:46 +0430

    catch errors when there is no active address

--- libraries/notifications/channel.php
+++ libraries/notifications/channel.php
@@ -5,6 +5,8 @@ use \packages\base\translator;
 use \packages\notifications;
 use \packages\email\api;
 use \packages\email\template;
+use \packages\email\deactivedAdressException;
+use \packages\email\defaultAddressException;
 class channel extends notifications\channel{
 	public function notify(event $event){
 		$lang = translator::getShortCodeLang();
@@ -12,13 +14,19 @@ class channel extends notifications\channel{
 		$template->where('name', $event->getName());
 		$template->where('lang', $lang);
 		if($template->has()){
-			foreach($event->getTargetUsers() as $user){
-				$api = new api();
-				$arguments = array_replace(array('user' => $user), $event->getArguments());
-				$api->template($event->getName(), $arguments);
-				$api->to($user->email, $user->getFullName());
-				$api->toUser($user);
-				$api->send();
+			try{
+				foreach($event->getTargetUsers() as $user){
+					$api = new api();
+					$arguments = array_replace(array('user' => $user), $event->getArguments());
+					$api->template($event->getName(), $arguments);
+					$api->to($user->email, $user->getFullName());
+					$api->toUser($user);
+					$api->send();
+				}
+			}catch(deactivedAdressException $e){
+				
+			}catch(defaultAddressException $e){
+				
 			}
 		}
 	}
