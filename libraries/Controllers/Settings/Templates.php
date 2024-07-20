9a9ad6a5c3ff1b4cb71b1dbb435d84b8015b0c21
Amir H. Yeganemehr <ygnmehr@gmail.com>
2024-07-20 17:33:39 +0330

    use base\Http instead of base\HTTP

--- libraries/Controllers/Settings/Templates.php
+++ libraries/Controllers/Settings/Templates.php
@@ -4,7 +4,7 @@ namespace packages\email\Controllers\Settings;
 
 use packages\base\DB\DuplicateRecord;
 use packages\base\DB\Parenthesis;
-use packages\base\HTTP;
+use packages\base\Http;
 use packages\base\InputValidation;
 use packages\base\NotFound;
 use packages\base\Translator;
